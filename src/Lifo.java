88d520a0c44b6e2b3067c141f3fcd6c12b29b7df
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-30 16:13:07 +0300

    ilk görev yapıldı

---
+++ src/Lifo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Lifo {
+    public static void main(String[] args) {
+
+        Deque<String> lifoExamples = new LinkedList<>();
+        lifoExamples.push("I did, did I?");
+        lifoExamples.push("Racecar");
+        lifoExamples.push("hello");
+        lifoExamples.push("Was it a car or a cat I saw ?");
+
+        for (String lifo : lifoExamples) {
+            String lifos = lifo.toLowerCase().replaceAll("[.,!?\"]", "");
+            String reversedLifos = new StringBuilder(lifos).reverse().toString();
+            if (lifos.equals(reversedLifos)) {
+                System.out.println("These are polindrom " + lifos);
+            } else {
+                System.out.println("Not polindrom " + lifos);
+            }
+        }
+    }
+}
